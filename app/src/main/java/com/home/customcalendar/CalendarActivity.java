0c8c6b23025f29ffdc80df2299e028d55e146d4a
陈先森 <1584935420@qq.com>
2021-04-12 00:08:32 +0800

    完善了很多。增加了参数的有效性和代码设置参数11

--- app/src/main/java/com/home/customcalendar/CalendarActivity.java
+++ app/src/main/java/com/home/customcalendar/CalendarActivity.java
@@ -14,15 +14,15 @@ public class CalendarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
         CustomCalendarView customCalendarView = findViewById(R.id.calendar);
-        customCalendarView.setDaySelectedBackgroundColor(Color.parseColor("#000000"));
-        customCalendarView.setShowAfterTodayDate(false);
-        customCalendarView.setDayTextColor(Color.parseColor("#A362DA"));
-        customCalendarView.setDayTextSize(10);
-        customCalendarView.setTitleTextColor(Color.parseColor("#A362DA"));
-        customCalendarView.setTitleTextSize(12);
-        customCalendarView.setWeekEndTextColor(Color.parseColor("#000000"));
-        customCalendarView.setSelectedBetweenBackgroundColor(Color.parseColor("#DA6262"));
-        customCalendarView.setTodayBackgroundColor(Color.parseColor("#DA6262"));
+//        customCalendarView.setDaySelectedBackgroundColor(Color.parseColor("#000000"));
+//        customCalendarView.setShowAfterTodayDate(false);
+//        customCalendarView.setDayTextColor(Color.parseColor("#A362DA"));
+//        customCalendarView.setDayTextSize(10);
+//        customCalendarView.setTitleTextColor(Color.parseColor("#A362DA"));
+//        customCalendarView.setTitleTextSize(12);
+//        customCalendarView.setWeekEndTextColor(Color.parseColor("#000000"));
+//        customCalendarView.setSelectedBetweenBackgroundColor(Color.parseColor("#DA6262"));
+//        customCalendarView.setTodayBackgroundColor(Color.parseColor("#DA6262"));
         customCalendarView.setOnDateSelected((startDate, endDate) -> {
             Toast.makeText(this, "开始" + startDate + "--结束" + endDate, Toast.LENGTH_SHORT).show();
         });
